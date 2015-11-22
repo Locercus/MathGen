f21fede79800326b2aa9b531de99cd8bbf24e342
Locercus <lo@cercus.net>
2015-11-22 15:59:42 +0100

    Read from stdin

--- gen.js
+++ gen.js
@@ -5,14 +5,39 @@
 require('./lib.js');
 
 const program = require('commander'),
-      util    = require('util');
+      util    = require('util'),
+      fs      = require('fs'),
+      path    = require('path');
 
 program
     .version('0.1.0')
-    .option('-l', '--lang [lang]', 'Language to output')
+    .usage('[options] <language>')
     .parse(process.argv);
 
+let language = path.join('lang', program.args[0] + '.js');
+let exists = false;
+try {
+    let stats = fs.lstatSync(language);
 
-let parser = new Parser();
+    if (stats.isFile())
+        exists = true;
+} catch (e) {}
 
-console.log(util.inspect(parser.p(parser.l('nthRoot(a^3 + b^3, 3)')), { depth: null }));
+if (!exists) {
+    console.error('MathGen: Supplied language does not exist');
+    process.exit(2);
+}
+
+process.stdin.setEncoding('utf8');
+process.stdin.resume();
+let input = [];
+process.stdin.on('data', function (chunk) {
+    input.push(chunk);
+});
+process.stdin.on('end', function () {
+    input = input.join();
+
+    let parser = new Parser();
+
+    console.log(util.inspect(parser.p(parser.l(input)), { depth: null }));
+});
