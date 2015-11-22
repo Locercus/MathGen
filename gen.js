adb1ae512bd1f569daf816d2930b9945292caad3
Locercus <lo@cercus.net>
2015-11-22 18:28:25 +0100

    Let's call that 1.0

--- gen.js
+++ gen.js
@@ -10,7 +10,7 @@ const program = require('commander'),
       path    = require('path');
 
 program
-    .version('0.1.0')
+    .version('1.0.0')
     .usage('[options] <language>')
     .parse(process.argv);
 
